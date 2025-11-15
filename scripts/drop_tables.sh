4e7af38d77a9824904fd3e059190d14416d6dd73
Aaron Jones <ajones2279@gmail.com>
2025-11-15 15:58:26 -0700

    Fix bug in drop script; add dump script

--- scripts/drop_tables.sh
+++ scripts/drop_tables.sh
@@ -6,6 +6,7 @@ DB_USER="postgres"
 echo "Dropping tables in \"$DB_NAME\"..."
 
 psql -U "$DB_USER" -d "$DB_NAME" <<EOF
+DROP TABLE IF EXISTS user_lego_sets CASCADE;
 DROP TABLE IF EXISTS user_set_pieces CASCADE;
 DROP TABLE IF EXISTS set_pieces CASCADE;
 DROP TABLE IF EXISTS lego_pieces CASCADE;
---
+++ scripts/dump_tables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DB_NAME="legodb"
+DB_USER="postgres"
+
+echo "Creating database \"$DB_NAME\"..."
+
+# Create the database (if it doesn't already exist)
+psql -U "$DB_USER" -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
+psql -U "$DB_USER" -c "CREATE DATABASE $DB_NAME"
+
+echo "Creating tables in \"$DB_NAME\"..."
+
+psql -U "$DB_USER" -d "$DB_NAME" <<EOF
+SELECT * FROM lego_sets;
+
+SELECT * FROM lego_pieces;
+
+SELECT * FROM set_pieces;
+
+SELECT * FROM user_set_pieces;
+
+SELECT * FROM user_lego_sets;
+EOF
