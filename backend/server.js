4d60474879104b3c217679a7c614d9300d377736
Aaron Jones <ajones2279@gmail.com>
2025-11-25 20:13:41 -0700

    Allow connection from remote hosts on LAN

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,4 @@ app.use(express.json());
 app.use('/api/sets', setRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
